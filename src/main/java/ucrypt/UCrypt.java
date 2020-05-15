2f3a5d2ae2814f7d38b5fe4a7b2feaf0b05a2ff6
-Unknown- <MrUnknownYT@gmail.com>
2020-05-14 23:09:18 -0600

    Added documentation

--- src/main/java/ucrypt/UCrypt.java
+++ src/main/java/ucrypt/UCrypt.java
@@ -4,14 +4,29 @@ import java.util.Random;
 
 public class UCrypt {
 	
+	/** Encodes the given string using a randomized cipher based around the given seed
+	 * @param seed The seed to use for the randomized cipher algorithm
+	 * @param str The string to encode
+	 * @return The given string encoded with a randomized cipher based around the given seed
+	 */
 	public static String encode(String seed, String str) {
 		return encode(seed.hashCode(), str);
 	}
 	
+	/** Decodes the given string using a reversed randomized cipher based around the given seed
+	 * @param seed The seed to use for the randomized cipher algorithm
+	 * @param str The string to decode
+	 * @return The given string decoded with a reversed randomized cipher based around the given seed
+	 */
 	public static String decode(String seed, String str) {
 		return decode(seed.hashCode(), str);
 	}
 	
+	/** Encodes the given string using a randomized cipher based around the given seed
+	 * @param seed The seed to use for the randomized cipher algorithm
+	 * @param str The string to encode
+	 * @return The given string encoded with a randomized cipher based around the given seed
+	 */
 	public static String encode(long seed, String str) {
 		Random r = new Random(seed);
 		StringBuilder sb = new StringBuilder();
@@ -24,6 +39,11 @@ public class UCrypt {
 		return sb.toString();
 	}
 	
+	/** Decodes the given string using a reversed randomized cipher based around the given seed
+	 * @param seed The seed to use for the randomized cipher algorithm
+	 * @param str The string to decode
+	 * @return The given string decoded with a reversed randomized cipher based around the given seed
+	 */
 	public static String decode(long seed, String str) {
 		Random r = new Random(seed);
 		StringBuilder sb = new StringBuilder();
